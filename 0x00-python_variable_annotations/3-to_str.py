38982a2c6ea3aee336397bbc045932ae284f34cf
Sarah Gad <sarahgad298@gmail.com>
2024-08-02 04:13:35 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module defines a to_str method"""
+
+
+def to_str(n: float) -> str:
+    """This fucntion returns the string representation of the float."""
+    return str(n)
