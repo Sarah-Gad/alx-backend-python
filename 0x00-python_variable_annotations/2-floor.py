31e04ec72dacb735af8f13dece014ccf61cf0b03
Sarah Gad <sarahgad298@gmail.com>
2024-08-02 04:01:01 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+"""This module defines a floor method"""
+
+
+def floor(n: float) -> int:
+    """This fucntion will get the floor on n"""
+    return math.floor(n)
