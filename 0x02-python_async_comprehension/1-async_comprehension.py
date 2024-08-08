5caec14e3f0b9b588ad00a767122e8431b67ad2f
Sarah Gad <sarahgad298@gmail.com>
2024-08-08 03:01:28 +0300

    edit annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """this module defines a coroutine"""
 async_generator = __import__('0-async_generator').async_generator
+from typing import List
 
-
-async def async_comprehension() -> list:
+async def async_comprehension() -> List[float]:
     """This fucntion will collect 0 random
     numbers using an async comprehensing over async_generator"""
     re_list = [i async for i in async_generator()]
